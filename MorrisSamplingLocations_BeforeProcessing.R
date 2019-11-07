4c9fcdabf429619e053ef2cf4fb1edee85859397
Jared <jds485@cornell.edu>
2019-11-06 20:32:12 -0500

    Morris sampling using the R sensitivity package

---
+++ MorrisSamplingLocations_BeforeProcessing.R
@@ -0,0 +1,34 @@
+#Script used to sample parameters for Morris SA of RHESSys streamflow-specific def parameters
+
+library(sensitivity)
+
+setwd("C:\\Users\\jsmif\\OneDrive - University of Virginia\\BES_Data\\BES_Data\\RHESSysFiles\\BR&POBR\\RHESSysFilePreparation\\defs\\MorrisSampleLocs")
+
+#Read in the sample data
+Problem = read.csv(file = 'BaismanMorrisSamplingProblemFile_Full.csv')
+
+#R3.5 seed - verified it's the same sample file in 5.0 and 5.3.
+set.seed(8154)
+MorrisSample = morris(model = function(x){1}, factors = as.character(Problem$NumberedParams), r = 40, design = list(type = "oat", levels = 100, grid.jump = 50), binf = Problem$Lower, bsup = Problem$Upper, scale = FALSE)
+
+#Write the sample file to a csv to be processed for checks on the input values using Python RHESSys model checks
+write.csv(MorrisSample$X, file = 'MorrisSamples_BeforeProcessing.csv', row.names = FALSE)
+
+#Sampling diagnostics
+test = morris(model = function(x){1}, factors = as.character(Problem$NumberedParams), r = 40, design = list(type = "oat", levels = 100, grid.jump = 50), binf = 0, bsup = 1, scale = FALSE)
+#Check visually for uniform distribution - this works when the scales are between 0 and 1
+png('50pctJumps.png', res = 200, width = 4, height = 4, units = 'in')
+hist(test$X, breaks = 100, xlab = 'X', main = '50% jumps')
+dev.off()
+
+test1 = morris(model = function(x){1}, factors = as.character(Problem$NumberedParams), r = 40, design = list(type = "oat", levels = 100, grid.jump = 20), binf = 0, bsup = 1, scale = FALSE)
+#Check visually for uniform distribution - this works when the scales are between 0 and 1
+png('20pctJumps.png', res = 200, width = 4, height = 4, units = 'in')
+hist(test1$X, breaks = 100, xlab = 'X', main = '20% jumps')
+dev.off()
+
+test2 = morris(model = function(x){1}, factors = as.character(Problem$NumberedParams), r = 40, design = list(type = "oat", levels = 100, grid.jump = 80), binf = 0, bsup = 1, scale = FALSE)
+#Check visually for uniform distribution - this works when the scales are between 0 and 1
+png('80pctJumps.png', res = 200, width = 4, height = 4, units = 'in')
+hist(test2$X, breaks = 100, xlab = 'X', main = '80% jumps')
+dev.off()
