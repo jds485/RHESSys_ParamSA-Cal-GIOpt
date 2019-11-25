9a48b16c217441384f438fab2d7c358ff0cd8f89
Jared <jds485@cornell.edu>
2019-11-25 12:25:18 -0500

    Adding shell script for extracting TN data using WRTDS and the transposed streamflow data

---
+++ ExtractTN.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+#SBATCH -D /scratch/js4yd/MorrisSA/TNprocessing/
+#SBATCH -o /scratch/js4yd/MorrisSA/TNprocessing/output/Run_%a.out
+#SBATCH --ntasks=1                              # Number of tasks per serial job (must be 1)
+#SBATCH -p standard           									# Queue partition name "standard"
+#SBATCH -A quinnlab       											# allocation name
+#SBATCH -t 00:30:00       											# Run time per serial job (hh:mm:ss) - up to 36 hours
+#SBATCH --mem-per-cpu=12288                     # Memory per cpu (Megabytes)
+#SBATCH --mail-user=js4yd@virginia.edu          # address for email notification
+#SBATCH --mail-type=ALL                  		    # email at Begin and End of job
+
+module load gcc/7.1.0 openmpi/3.1.4 R/3.5.3
+
+#Output and input file directories
+OUTDIR='/scratch/js4yd/MorrisSA/TNprocessing/TNdata'
+INDIR='/scratch/js4yd/MorrisSA/TNprocessing'
+
+Rscript "$INDIR"/TNFileExtraction.R "$SLURM_ARRAY_TASK_ID" "$INDIR" "$OUTDIR"
