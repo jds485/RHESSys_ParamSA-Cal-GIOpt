a9f49c73fcd6849d43737c05387ac9a0e161a441
Jared D Smith <jared.d.smith485@gmail.com>
2019-11-25 12:08:34 -0500

    Rearranged for running on Rivanna

--- TNFileExtraction.R
+++ TNFileExtraction.R
@@ -1,29 +1,50 @@
 #TN extraction on Rivanna
 
+library(EGRET)
+library(survival)
+library(pracma)
+
 #Get the arg[1] value, which is the date column number to use
+arg = commandArgs(trailingOnly = TRUE)
 d = arg[1]
 
-#Load in the columns (arg[2] is directory of file)
+#Load in the modified code (arg[2] is directory of file)
 setwd(arg[2])
-BasinSF_cols = read.table(file = 'SAResults_BasinStreamflow_p4.txt', sep = '\t', stringsAsFactors = FALSE, header = TRUE, check.names = FALSE, nrows = 1)
+source('WRTDS_modifiedFunctions.R')
 
-BasinSF = read.table(file = 'SAResults_HillStreamflow_p6.txt', sep = '\t', stringsAsFactors = FALSE, header = TRUE, check.names = FALSE, colClasses = c('numeric', rep(NULL,ncol(BasinSF_cols)-2)))
-HillSF = read.table(file = 'SAResults_HillStreamflow_p6.txt', sep = '\t', stringsAsFactors = FALSE, header = TRUE, check.names = FALSE)
+#Load in the transposed streamflow data with rows as the dates
+BasinSF = read.table(file = 'SAResults_BasinStreamflow_p4_t.txt', sep = '\t', stringsAsFactors = FALSE, check.names = FALSE, nrows = 1, skip=d+1, row.names = 1)
+HillSF = read.table(file = 'SAResults_HillStreamflow_p6_t.txt', sep = '\t', stringsAsFactors = FALSE, check.names = FALSE, nrows = 1, skip=d+2, row.names = 1)
+
+#Tables
+TabInt = as.matrix(read.table(file = 'TabIntMod4_p5.txt', sep = '\t', header = TRUE, check.names = FALSE))
+TabYear = as.matrix(read.table(file = 'TabYearMod4_p4.txt', sep = '\t', header = TRUE, check.names = FALSE))
+TabLogQ = as.matrix(read.table(file = 'TabLogQMod4_p4.txt', sep = '\t', header = TRUE, check.names = FALSE))
+TabSinYear = as.matrix(read.table(file = 'TabSinYearMod4_p4.txt', sep = '\t', header = TRUE, check.names = FALSE))
+TabCosYear = as.matrix(read.table(file = 'TabCosYearMod4_p4.txt', sep = '\t', header = TRUE, check.names = FALSE))
+TabLogErr = as.matrix(read.table(file = 'TabLogErrMod4_p5.txt', sep = '\t', header = TRUE, check.names = FALSE))
 
 #Loop over the date to fill in the new dataset
-a = matrix(NA, nrow = 3, ncol = ncol(BasinSF)-1)
-h = matrix(NA, nrow = 3, ncol = ncol(BasinSF)-1)
+a = matrix(NA, nrow = 3, ncol = ncol(BasinSF))
+h = matrix(NA, nrow = 3, ncol = ncol(BasinSF))
 rowt = as.numeric(rownames(TabInt))
 colt = as.numeric(colnames(TabInt))
-dtest = as.character(colnames(BasinSF)[d+1])
-a = apply(X = t(BasinSF[,d+1]), MARGIN = 2, FUN = predictWRTDS, Date = dtest, rowt = rowt, colt = colt)
-h = apply(X = t(HillSF[,d+2]), MARGIN = 2, FUN = predictWRTDS, Date = dtest, rowt = rowt, colt = colt)
-  
-#Write to txt file
-write.table(x = a, file = paste0(arg[3],'/a_', d, '.txt'), sep = '\t')
-write.table(x = h, file = paste0(arg[3],'/h_', d, '.txt'), sep = '\t')
+dtest = rownames(BasinSF)
+a = apply(X = BasinSF, MARGIN = 2, FUN = predictWRTDS, Date = dtest, rowt = rowt, colt = colt)
+h = apply(X = HillSF, MARGIN = 2, FUN = predictWRTDS, Date = dtest, rowt = rowt, colt = colt)
 
-tic = Sys.time()
-BasinSF = read.table(file = 'SAResults_HillStreamflow_p6.txt', sep = '\t', stringsAsFactors = FALSE, header = TRUE, check.names = FALSE)
-toc = Sys.time()
-print(toc-tic)
+#Write to txt file (arg[3] is output directory)
+write.table(x = a, file = paste0(arg[3],'/a_', d, '.txt'), sep = '\t', col.names = FALSE, row.names = FALSE)
+write.table(x = h, file = paste0(arg[3],'/h_', d, '.txt'), sep = '\t', col.names = FALSE, row.names = FALSE)
+
+# tic = Sys.time()
+# d = 3900
+# BasinSF = read.table(file = 'SAResults_BasinStreamflow_p4_t.txt', sep = '\t', stringsAsFactors = FALSE, check.names = FALSE, nrows = 1, skip=d+1, row.names = 1)
+# rowt = as.numeric(rownames(TabInt))
+# colt = as.numeric(colnames(TabInt))
+# dtest = rownames(BasinSF)
+# a = apply(X = BasinSF, MARGIN = 2, FUN = predictWRTDS, Date = dtest, rowt = rowt, colt = colt)
+# HillSF = read.table(file = 'SAResults_HillStreamflow_p6_t.txt', sep = '\t', stringsAsFactors = FALSE, check.names = FALSE, nrows = 1, skip=d+2, row.names = 1)
+# h = apply(X = HillSF, MARGIN = 2, FUN = predictWRTDS, Date = dtest, rowt = rowt, colt = colt)
+# toc = Sys.time()
+# print(toc-tic)
