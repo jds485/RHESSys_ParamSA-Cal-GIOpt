c68e37c46941a0b3e603fae42f0dc791931631ea
Jared D Smith <jared.d.smith485@gmail.com>
2019-12-02 11:25:39 -0500

    Hillslope reordering completed

--- ReorderingSAresults.R
+++ ReorderingSAresults.R
@@ -37,21 +37,31 @@ write.table(HillSF, file = 'SAResults_HillStreamflow_p6_Reordered.txt', row.name
 
 #Make sure that this reordered data matches the re-run data with correct indexing
 setwd("C:\\Users\\js4yd\\OneDrive - University of Virginia\\BES_Data\\BES_Data\\RHESSysFiles\\BR&POBR")
-BasinSF = read.table(file = 'SAResults_BasinStreamflow_p4_Reordered.txt', row.names = FALSE, sep = '\t', header = TRUE, stringsAsFactors = FALSE)
-HillSF = read.table(file = 'SAResults_HillStreamflow_p6_Reordered.txt', row.names = FALSE, sep = '\t', header = TRUE, stringsAsFactors = FALSE)
-BasinSF_Corr = read.table(file = 'SAResults_BasinStreamflow_p4_CorrOrder.txt', row.names = FALSE, sep = '\t', header = TRUE, stringsAsFactors = FALSE)
-HillSF_Corr = read.table(file = 'SAResults_HillStreamflow_p6_CorrOrder.txt', row.names = FALSE, sep = '\t', header = TRUE, stringsAsFactors = FALSE)
+BasinSF = read.table(file = 'SAResults_BasinStreamflow_p4_Reordered.txt', sep = '\t', header = TRUE, stringsAsFactors = FALSE, check.names = FALSE)
+HillSF = read.table(file = 'SAResults_HillStreamflow_p6_Reordered.txt', sep = '\t', header = TRUE, stringsAsFactors = FALSE, check.names = FALSE)
+setwd("C:\\Users\\js4yd\\Documents\\BaismanSA\\RHESSysRuns")
+BasinSF_Corr = read.table(file = 'SAResults_BasinStreamflow_p4_CorrOrder.txt', sep = '\t', header = TRUE, stringsAsFactors = FALSE, check.names = FALSE)
+HillSF_Corr = read.table(file = 'SAResults_HillStreamflow_p6_CorrOrder.txt',  sep = '\t', header = TRUE, stringsAsFactors = FALSE, check.names = FALSE)
 
 test = which((BasinSF - BasinSF_Corr) !=0)
+#Fixme: Some issue with rounding. Should only need 6 decimal places, but they were different to that decimal place
+#It seems it's a problem with the original Hillstreamflow_p6 file - it has 5 decimal places for some values and 6 for others. May have been a scipen issue when the text file was written
+test = which((round(HillSF,4) - round(HillSF_Corr,4)) !=0)
 
+for (h in 1:14){
+  t1 = round(HillSF[HillSF$HillID == h,],4)
+  t2 = round(HillSF_Corr[HillSF_Corr$HillID == h,],4)
+  
+  test = which((t1[,-c(1,2)] - t2[,-c(1,2)]) !=0)
+  
+  print(test)
+}
 
 #Now also reorder the TN results
+setwd("C:\\Users\\js4yd\\OneDrive - University of Virginia\\BES_Data\\BES_Data\\RHESSysFiles\\BR&POBR")
 BasinTN05 = read.table(file = 'SAResults_BasinTN05_p3_All.txt', sep = '\t', stringsAsFactors = FALSE, header = TRUE, check.names = FALSE)
 BasinTNMed = read.table(file = 'SAResults_BasinTNMed_p3_All.txt', sep = '\t', stringsAsFactors = FALSE, header = TRUE, check.names = FALSE)
 BasinTN95 = read.table(file = 'SAResults_BasinTN95_p3_All.txt', sep = '\t', stringsAsFactors = FALSE, header = TRUE, check.names = FALSE)
-HillTN05 = read.table(file = 'SAResults_HillTN05_p3_All.txt', sep = '\t', stringsAsFactors = FALSE, header = TRUE, check.names = FALSE)
-HillTNMed = read.table(file = 'SAResults_HillTNMed_p3_All.txt', sep = '\t', stringsAsFactors = FALSE, header = TRUE, check.names = FALSE)
-HillTN95 = read.table(file = 'SAResults_HillTN95_p3_All.txt', sep = '\t', stringsAsFactors = FALSE, header = TRUE, check.names = FALSE)
 
 #Reorder according to the correct Morris order
 BasinTN05 = BasinTN05[OrdInds, ]
@@ -59,6 +69,16 @@ BasinTNMed = BasinTNMed[OrdInds, ]
 BasinTN95 = BasinTN95[OrdInds, ]
 #Get the new replicates named correctly
 BasinTN05$Replicate = BasinTNMed$Replicate = BasinTN95$Replicate = seq(1,10880,1)
+
+write.table(BasinTN05, file = 'SAResults_BasinTN05_p3_All_Reordered.txt', row.names = FALSE, sep = '\t')
+write.table(BasinTNMed, file = 'SAResults_BasinTNMed_p3_All_Reordered.txt', row.names = FALSE, sep = '\t')
+write.table(BasinTN95, file = 'SAResults_BasinTN95_p3_All_Reordered.txt', row.names = FALSE, sep = '\t')
+
+rm(BasinTN05, BasinTN95, BasinTNMed)
+
+HillTN05 = read.table(file = 'SAResults_HillTN05_p3_All.txt', sep = '\t', stringsAsFactors = FALSE, header = TRUE, check.names = FALSE)
+HillTNMed = read.table(file = 'SAResults_HillTNMed_p3_All.txt', sep = '\t', stringsAsFactors = FALSE, header = TRUE, check.names = FALSE)
+HillTN95 = read.table(file = 'SAResults_HillTN95_p3_All.txt', sep = '\t', stringsAsFactors = FALSE, header = TRUE, check.names = FALSE)
 
 #Reorder according to the correct Morris order and rename the replicates
 for (h in 1:14){
@@ -70,9 +90,6 @@ for (h in 1:14){
   HillTN95[(1+10880*(h-1)):(1+10880*(h-1)+10879),]$Replicate = seq(1,10880,1)
 }
 
-write.table(BasinTN05, file = 'SAResults_BasinTN05_p3_All_Reordered.txt', row.names = FALSE, sep = '\t')
-write.table(BasinTNMed, file = 'SAResults_BasinTNMed_p3_All_Reordered.txt', row.names = FALSE, sep = '\t')
-write.table(BasinTN95, file = 'SAResults_BasinTN95_p3_All_Reordered.txt', row.names = FALSE, sep = '\t')
 write.table(HillTN05, file = 'SAResults_HillTN05_p3_All_Reordered.txt', row.names = FALSE, sep = '\t')
 write.table(HillTNMed, file = 'SAResults_HillTNMed_p3_All_Reordered.txt', row.names = FALSE, sep = '\t')
 write.table(HillTN95, file = 'SAResults_HillTN95_p3_All_Reordered.txt', row.names = FALSE, sep = '\t')
