bbc06007d3166b5045ae24c8402a6345986f25ef
Jared D Smith <jared.d.smith485@gmail.com>
2019-12-02 21:13:32 -0500

    Modified runtime specification for JoinTN shell scripts

--- JoinTN.sh
+++ JoinTN.sh
@@ -4,7 +4,7 @@
 #SBATCH --ntasks=1                              # Number of tasks per serial job (must be 1)
 #SBATCH -p largemem           									# Queue partition name
 #SBATCH -A quinnlab       											# allocation name
-#SBATCH -t 04:00:00       											# Run time per serial job (hh:mm:ss) - up to 36 hours
+#SBATCH -t 01:00:00       											# Run time per serial job (hh:mm:ss) - up to 36 hours
 #SBATCH --mail-user=js4yd@virginia.edu          # address for email notification
 #SBATCH --mail-type=ALL                  		    # email at Begin and End of job
 
