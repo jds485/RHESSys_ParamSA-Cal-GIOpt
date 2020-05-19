3239252d55b37a76b43b29d85712389748217b7a
Jared D Smith <jared.d.smith485@gmail.com>
2020-05-19 11:25:59 -0400

    Added success and message to the output info.

--- LikelihoodData/collectParams.py
+++ LikelihoodData/collectParams.py
@@ -1,7 +1,7 @@
 import pandas as pd
 
 #Fixme: Could use 1 loop in this script instead of 2.
-Qdf = pd.DataFrame(columns=['Replicate','beta','xi','sigma_0','sigma_1','phi_1','mu_h','logL','SSE'])
+Qdf = pd.DataFrame(columns=['Replicate','beta','xi','sigma_0','sigma_1','phi_1','mu_h','logL','SSE','success','mess'])
 nprocs = 300
 for proc in range(nprocs):
 	procParams = pd.read_csv('SA_Params_logL_Baisman_Flow_rank' + str(proc) + '.csv')
@@ -9,7 +9,7 @@ for proc in range(nprocs):
 
 Qdf.to_csv('SA_Params_logL_Baisman_Flow.csv')
 
-TNdf = pd.DataFrame(columns=['Replicate','beta','xi','sigma_0','sigma_1','phi_1','mu_h','logL','SSE'])
+TNdf = pd.DataFrame(columns=['Replicate','beta','xi','sigma_0','sigma_1','phi_1','mu_h','logL','SSE','success','mess'])
 nprocs = 300
 for proc in range(nprocs):
 	procParams = pd.read_csv('SA_Params_logL_Baisman_TN_rank' + str(proc) + '.csv')
