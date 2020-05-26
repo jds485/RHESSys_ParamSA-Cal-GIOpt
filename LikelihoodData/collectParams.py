7e61c1d8ba0ae0065384ec09a78b994c1e4b695b
Jared D Smith <jared.d.smith485@gmail.com>
2020-05-26 10:15:51 -0400

    Changed saved file names, and number of processors used

--- LikelihoodData/collectParams.py
+++ LikelihoodData/collectParams.py
@@ -2,17 +2,17 @@ import pandas as pd
 
 #Fixme: Could use 1 loop in this script instead of 2.
 Qdf = pd.DataFrame(columns=['Replicate','beta','xi','sigma_0','sigma_1','phi_1','mu_h','logL','SSE','success','mess'])
-nprocs = 300
+nprocs = 400
 for proc in range(nprocs):
 	procParams = pd.read_csv('SA_Params_logL_Baisman_Flow_rank' + str(proc) + '.csv')
 	Qdf = pd.concat([Qdf,procParams])
 
-Qdf.to_csv('SA_Params_logL_Baisman_Flow.csv')
+Qdf.to_csv('SA_Params_logL_Baisman_Flow_SQL_max1000_20samps.csv', index=False)
 
 TNdf = pd.DataFrame(columns=['Replicate','beta','xi','sigma_0','sigma_1','phi_1','mu_h','logL','SSE','success','mess'])
 nprocs = 300
 for proc in range(nprocs):
 	procParams = pd.read_csv('SA_Params_logL_Baisman_TN_rank' + str(proc) + '.csv')
-	TNdf = pd.concat([TNdf,procParams])
+	TNdf = pd.concat([TNdf,procParams], sort=False)
 
-TNdf.to_csv('SA_Params_logL_Baisman_TN.csv')
+TNdf.to_csv('SA_Params_logL_Baisman_TN_SQL_max1000_20samps.csv', index=False)
