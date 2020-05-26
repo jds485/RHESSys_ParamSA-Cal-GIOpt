29a41fd4d540d986b79e87652d19661f03f94b9f
Jared D Smith <jared.d.smith485@gmail.com>
2020-05-26 10:16:27 -0400

    Add red points to plots where convergence did not succeed. Changed save file names.

--- LikelihoodData/plot_logL_v_SSE.py
+++ LikelihoodData/plot_logL_v_SSE.py
@@ -2,7 +2,7 @@ import pandas as pd
 import numpy as np
 from matplotlib import pyplot as plt
 
-Qdf = pd.read_csv('SA_Params_logL_Baisman_Flow.csv')
+Qdf = pd.read_csv('SA_Params_logL_Baisman_Flow_SQL_max1000_20samps.csv')
 
 logLranks = np.argsort(Qdf['logL'])[::-1]
 SSEranks = np.argsort(Qdf['SSE'])
@@ -14,13 +14,14 @@ plt.savefig('Flow_Ranks_logL_v_SSE.png')
 plt.clf()
 
 plt.scatter(-Qdf['logL'],Qdf['SSE'])
+plt.scatter(-Qdf.iloc[np.where(Qdf['success'] == False)[0],:]['logL'],Qdf.iloc[np.where(Qdf['success'] == False)[0],:]['SSE'], c='red')
 plt.title('Pearson Correlation= ' + str(np.corrcoef(-Qdf['logL'],Qdf['SSE'])[0,1]))
 plt.xlabel('-logL (Flow)')
 plt.ylabel('SSE (Flow)')
 plt.savefig('Flow_logL_v_SSE.png')
 plt.clf()
 
-TNdf = pd.read_csv('SA_Params_logL_Baisman_TN.csv')
+TNdf = pd.read_csv('SA_Params_logL_Baisman_TN_SQL_max1000_20samps.csv')
 
 logLranks = np.argsort(TNdf['logL'])[::-1]
 SSEranks = np.argsort(TNdf['SSE'])
@@ -28,12 +29,13 @@ plt.scatter(logLranks,SSEranks)
 plt.title('Spearman Correlation= ' + str(np.corrcoef(logLranks,SSEranks)[0,1]))
 plt.xlabel('Ranks by logL (TN)')
 plt.ylabel('Ranks by SSE (TN)')
-plt.savefig('TN_Ranks_logL_v_SSE.png')
+plt.savefig('TN_Ranks_logL_v_SSE_20samps.png')
 plt.clf()
 
 plt.scatter(-TNdf['logL'],TNdf['SSE'])
+plt.scatter(-TNdf.iloc[np.where(TNdf['success'] == False)[0],:]['logL'],TNdf.iloc[np.where(TNdf['success'] == False)[0],:]['SSE'], c='red')
 plt.title('Pearson Correlation= ' + str(np.corrcoef(-TNdf['logL'],TNdf['SSE'])[0,1]))
 plt.xlabel('-logL (TN)')
 plt.ylabel('SSE (TN)')
-plt.savefig('TN_logL_v_SSE.png')
+plt.savefig('TN_logL_v_SSE_20samps.png')
 plt.clf()
